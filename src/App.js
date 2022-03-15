175d0e307d843ba6e93711ebc1def2d02fcdba9e
고진규 <gojingyu@gojingyus-MacBook-Air.local>
2022-03-15 16:55:17 +0900

    {커밋 메시지 넣기}

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav, NavDropdown, Button, Dropdown,CloseButton } from 'react-bootstrap';
+import { RotateCw } from 'react-feather';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Navbar bg="white" expand="lg">
+        <Container className='nav' >
+          <Navbar.Brand href="#home">FunktionsQL</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* nav */}
+
+      <hr id='hr'/>
+      <div className='lang'>
+        <div className='tras'>
+          <div className="row" >
+            <div className='col-5'   >
+              <div class="dropdown">
+                <div className='langselect'>언어선택 ▼</div>
+                <div class="dropdown-content">
+                  <div class="container">
+                    <div class="row align-items-start">
+                      <div class="col dropdown-item">c</div>
+                      <div class="col dropdown-item">java</div>
+                      <div class="col dropdown-item">swift</div>
+                    </div>
+                    <div class="row align-items-center">
+                      <div class="col dropdown-item">python</div>
+                      <div class="col dropdown-item">javascript</div>
+                      <div class="col dropdown-item">node</div>
+                    </div>
+                    <div class="row align-items-end">
+                      <div class="col dropdown-item">jsp</div>
+                      <div class="col dropdown-item">something</div>
+                      <div class="col dropdown-item">someone</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-1'>
+            <button id='rotatebtn'><RotateCw /></button>
+            </div>
+            {/* 언어선택1 */}
+            
+            <div className='col-6' >
+              <div class="dropdown">
+                <div className='langselect' >언어선택 ▼</div>
+                <div class="dropdown-content">
+                  <a class="dropdown-item">java</a>
+                  <a class="dropdown-item">c</a>
+                  <a class="dropdown-item">swift</a>
+                  <a class="dropdown-item">python</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* 언어선택2 */}
+        
+
+        <div className="row" >
+          <div className="col-6" id='inner'>
+            <div contentEditable="true" class='text' placeholder="input something" >
+            </div>
+            <Button variant="outline-success" id="translate-btn">번역</Button>
+          </div>
+          <div class="col-6" id='inner'>
+            <div contentEditable="true" class='text' placeholder="" id="second-box" ></div> 
+          </div>
+        </div>
+      </div>
+      {/* 입력창 */}
+      
+
+      <hr/>
+      <footer >
+        <Container>
+          <div style={{ textAlign: 'center' }}>
+            FunktionsQL<br/>
+            Copyright © FunktionsQL Corp. All rights reserved.
+          </div>
+        </Container>
+      </footer>
+      {/* footer */}
     </div>
   );
 }
-
 export default App;
+
